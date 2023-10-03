b3db626238c4e56e36f1dbbc27fbffb83c529704
ahmaddaniswara01 <124378481+ahmaddaniswara01@users.noreply.github.com>
2023-10-03 07:10:15 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,23 +1,4 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'; // Import index.js
+import { createApp } from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false;
-vue.use(VueRouter)
-
-// Import komponen untuk route
-import TodoList from './components/TodoList.vue'
-import HelloWorld from './components/HelloWorld.vue'
-import NotFound from './components/NotFound.vue'
-
-const routes = [
-    { path: '/', component: HelloWorld },
-    { path: '/todo', component: TodoList },
-    { path: '*', component: NotFound } // Rute ini cocok jika tidak ada rute yang cocok
-  ]
-
-new Vue({
-  render: (h) => h(App),
-  router, // 
-
-}).mount('#app')
+createApp(App).mount("#app");
