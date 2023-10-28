cf7e017dd6baaa020f4b2824f555a826da130acf
ahmaddaniswara01 <124378481+ahmaddaniswara01@users.noreply.github.com>
2023-10-28 14:41:23 +0700

    Update Todo.js adding hook

--- backend/src/collections/Todo.js
+++ backend/src/collections/Todo.js
@@ -1,3 +1,4 @@
+import payload from "payload";
 /** @type {import('payload/types').CollectionConfig} */
 const Todo = {
   slug: "todo",
@@ -6,7 +7,44 @@ const Todo = {
   },
   access: {
     read: () => true,
+    update: () => true,
+    delete: () => true,
+    create: () => true,
   },
+  hooks: {
+        afterOperation: [
+            async (args) => {
+                if (args.operation == "create") {
+                    payload.create({
+                        collection: "Log",
+                        data: {
+                            collectionName: "todo",
+                            action : "create",
+                            timestamp: new Date()
+                        },
+                    });
+                } else if (args.operation == "update") {
+                    payload.create({
+                        collection: "Log",
+                        data: {
+                            collectionName: "todo",
+                            action : "update",
+                            timestamp: new Date()
+                        },
+                    });
+                } else if (args.operation == "delete") {
+                    payload.create({
+                        collection: "Log",
+                        data: {
+                            collectionName: "todo",
+                            action : "delete",
+                            timestamp: new Date()
+                        },
+                    });
+                } 
+            },
+        ],
+    },
   fields: [
     {
       name: "title",
